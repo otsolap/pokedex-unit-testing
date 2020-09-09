40dbc2e1da0c618fdc5187dd97dde0028986e410
OtsoLap <59653348+otsolap@users.noreply.github.com>
2020-09-09 10:07:57 +0300

    made hoang's pokedex into a test unit app.

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme'
+
+// toBe tai toEqual
+//eli expect = olettaa, toequal = tulos. vähän niinkuin 2+2 =4.
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+ const wrap = shallow(<App></App>);
+ expect(false).toEqual(true)
 });
+
+
+test('renders learn react link', () => {
+  const wrap = shallow(<App></App>);
+  console.log(wrap.debug())
+ });
